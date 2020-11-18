11c0211c72c9e90836bfda8f08e11772afae79aa
Dslepchenko <dslepchenko@gmail.com>
2020-11-18 14:54:18 +0200

    разбивает на модули

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-const DAY_NUMBER = 1;
 const QUANTITY_PINS = 3;
 import { createFormTemplate } from "./view/trip-form-template.js";
 import { createTripInfoTemplate } from "./view/info-trip-template.js";
@@ -6,8 +5,6 @@ import { createTripCostTemplate } from "./view/trip-cost-template.js";
 import { createNavTemplate } from "./view/nav-template.js";
 import { createFiltersTemplate } from "./view/trip-filters.js";
 import { createTripSortTemplate } from "./view/trip-sort-template.js";
-
-import { createDayCounterTemplate } from "./view/trip-day-counter-template.js";
 import { createDayTemplate } from "./view/trip-day-template.js";
 import { createPointTemplate } from "./view/trip-point-template.js";
 
@@ -23,22 +20,19 @@ const tripCostElement = tripElement.querySelector(`.trip-info`);
 render(tripCostElement, createTripCostTemplate(), `beforeend`);
 
 const tripControls = tripElement.querySelector(`.trip-controls`);
+
+
 const firstElement = tripControls.querySelector(`:first-child`);
 render(firstElement, createNavTemplate(), `afterend`);
 render(tripControls, createFiltersTemplate(), `beforeend`);
-
 const tripEvents = document.querySelector(`.trip-events`);
 render(tripEvents, createTripSortTemplate(), `afterbegin`);
-render(tripEvents, createFormTemplate(), `afterbegin`);
 
-render(tripEvents, createDayCounterTemplate(), `beforeend`);
+render(tripEvents, createFormTemplate(), `beforeend`);
 
-const dayContainer = tripEvents.querySelector(`.trip-days`);
+render(tripEvents, createDayTemplate(), `beforeend`);
 
-for (let i = 0; i < DAY_NUMBER; i++) {
-    render(dayContainer, createDayTemplate(), `beforeend`);
-    const tripEventsContainers = tripEvents.querySelectorAll(`.trip-events__list`);
-    for (let k = 0; k < QUANTITY_PINS; k++) {
-        render(tripEventsContainers[i], createPointTemplate(), `beforeend`);
-    }
+const tripDayEventsContainer = tripEvents.querySelector(`.trip-events__list`);
+for (let i = 0; i < QUANTITY_PINS; i++) {
+    render(tripDayEventsContainer, createPointTemplate(), `beforeend`);
 }
--- src/view/trip-day-counter-template.js
+++
@@ -1,4 +0,0 @@
-export const createDayCounterTemplate = () => {
-    return `<ul class="trip-days"></ul>`;
-
-};
